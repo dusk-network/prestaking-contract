8ee49650ca7cb305a9241bcf8f16eff6ca8293c5
Jules de Smit <julesdesmit@gmail.com>
2020-07-23 12:13:19 +0200

    Update DuskToken contract to no longer be abstract
    This was causing issues when trying to run truffles testing command

--- contracts/DuskToken.sol
+++ contracts/DuskToken.sol
@@ -4,10 +4,10 @@ pragma solidity 0.6.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-abstract contract DuskToken is ERC20 {
+contract DuskToken is ERC20 {
     using SafeMath for uint256;
     
-    constructor() public {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) public {
         // Send all 500,000,000 tokens to contract creator
         _mint(msg.sender, uint256(500000000).mul(10**uint(18)));
     }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var DuskToken = artifacts.require("./DuskToken.sol");
 var Prestaking = artifacts.require("./Prestaking.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DuskToken).then(function() {
+  deployer.deploy(DuskToken, "Dusk Network", "DUSK").then(function() {
     return deployer.deploy(Prestaking, DuskToken.address, 250000, 250000, 200);
   });
 };
