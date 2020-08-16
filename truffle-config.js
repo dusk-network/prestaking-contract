c316cc48521fb817c2a28ae00378867d215a298c
Jules de Smit <julesdesmit@gmail.com>
2020-08-16 12:58:13 +0200

    Set normal testing block gas limit to 10 million
    This is primarily used to test the limits of the
    contract, to see what we can afford to onboard without
    exceeding the block gas limit of the mainnet.

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 0x989680, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
     },
     coverage: {
       host: "localhost",
